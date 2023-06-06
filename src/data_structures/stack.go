84759cd9b5edaa72968f4df9c96188e22e134c50
n-shkaruba <n-shkaruba@yandex-team.ru>
2023-06-06 09:39:27 +0200

    Add stack data structure

---
+++ src/data_structures/stack.go
@@ -0,0 +1,27 @@
+package data_structures
+
+type Stack[T any] struct {
+	arr []T
+}
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{}
+}
+
+func (s *Stack[T]) Push(value T) {
+	s.arr = append(s.arr, value)
+}
+
+func (s *Stack[T]) Pop() T {
+	val := s.arr[len(s.arr)-1]
+	s.arr = s.arr[:len(s.arr)-1]
+	return val
+}
+
+func (s *Stack[T]) Peek() T {
+	return s.arr[len(s.arr)-1]
+}
+
+func (s *Stack[T]) GetSize() int {
+	return len(s.arr)
+}
---
+++ src/data_structures/stack_test.go
@@ -0,0 +1,73 @@
+package data_structures
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStack_PushPop(t *testing.T) {
+	s := NewStack[int]()
+
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+
+	assert.Equal(t, 3, s.Pop())
+	assert.Equal(t, 2, s.Pop())
+	assert.Equal(t, 1, s.Pop())
+}
+
+func TestStack_Peek(t *testing.T) {
+	s := NewStack[int]()
+
+	s.Push(1)
+	assert.Equal(t, 1, s.Peek())
+
+	s.Push(2)
+	assert.Equal(t, 2, s.Peek())
+
+	s.Push(3)
+	assert.Equal(t, 3, s.Peek())
+}
+
+func TestStack_GetSize(t *testing.T) {
+	s := NewStack[int]()
+
+	s.Push(1)
+	assert.Equal(t, 1, s.GetSize())
+
+	s.Push(1)
+	assert.Equal(t, 2, s.GetSize())
+
+	s.Pop()
+	assert.Equal(t, 1, s.GetSize())
+
+	s.Pop()
+	assert.Equal(t, 0, s.GetSize())
+}
+
+func TestStack_DifferentGenericTypes(t *testing.T) {
+	intStack := NewStack[int]()
+	intStack.Push(1)
+	assert.Equal(t, 1, intStack.Pop())
+
+	floatStack := NewStack[float64]()
+	floatStack.Push(1.0)
+	assert.Equal(t, 1.0, floatStack.Pop())
+
+	stringStack := NewStack[string]()
+	stringStack.Push("test")
+	assert.Equal(t, "test", stringStack.Pop())
+
+	boolStack := NewStack[bool]()
+	boolStack.Push(true)
+	assert.Equal(t, true, boolStack.Pop())
+
+	type person struct {
+		name string
+		age  int
+	}
+	customStructStack := NewStack[person]()
+	customStructStack.Push(person{"test", 123})
+	assert.Equal(t, person{"test", 123}, customStructStack.Pop())
+}
