a39d4c1a3db3ccfb2d65f47353b7faae4268dbc7
n-shkaruba <n-shkaruba@yandex-team.ru>
2023-06-09 22:49:05 +0200

    Make SinglyLinkedListNode fields exported, add comments to singly_linked_list.go components

--- src/data_structures/singly_linked_list.go
+++ src/data_structures/singly_linked_list.go
@@ -2,30 +2,35 @@ package data_structures
 
 ////////////////////// Singly Linked List //////////////////////
 
+// SinglyLinkedListNode is a linked list node with a pointer to the next node
 type SinglyLinkedListNode[T any] struct {
-	val  T
-	next *SinglyLinkedListNode[T]
+	Val  T
+	Next *SinglyLinkedListNode[T]
 }
 
-func NewSinglyLinkedListNode[T any](val T) *SinglyLinkedListNode[T] {
+// NewSinglyLinkedListNode works in O(1) time, O(1) space
+func NewSinglyLinkedListNode[T any](val T, next *SinglyLinkedListNode[T]) *SinglyLinkedListNode[T] {
 	return &SinglyLinkedListNode[T]{
-		val: val,
+		Val:  val,
+		Next: next,
 	}
 }
 
+// NewSentinelSinglyLinkedListNode works in O(1) time, O(1) space
 func NewSentinelSinglyLinkedListNode[T any]() *SinglyLinkedListNode[T] {
 	return &SinglyLinkedListNode[T]{
-		val: *new(T),
+		Val: *new(T),
 	}
 }
 
+// NewSinglyLinkedList works in O(n) time, O(n) space
 func NewSinglyLinkedList[T any](arr []T) *SinglyLinkedListNode[T] {
 	headSentinel := NewSentinelSinglyLinkedListNode[T]()
 
 	cur := headSentinel
 	for _, e := range arr {
-		cur.next = NewSinglyLinkedListNode[T](e)
-		cur = cur.next
+		cur.Next = NewSinglyLinkedListNode[T](e, nil)
+		cur = cur.Next
 	}
 
 	return headSentinel
--- src/data_structures/singly_linked_list_test.go
+++ src/data_structures/singly_linked_list_test.go
@@ -5,62 +5,66 @@ import (
 	"testing"
 )
 
-func TestNewSinglyLinkedListNode(t *testing.T) {
-	node := NewSinglyLinkedListNode(1)
-	assert.Equal(t, 1, node.val)
-	assert.Nil(t, node.next)
+func TestSinglyLinkedListNode_New(t *testing.T) {
+	firstNode := NewSinglyLinkedListNode(1, nil)
+	assert.Equal(t, 1, firstNode.Val)
+	assert.Nil(t, firstNode.Next)
+
+	secondNode := NewSinglyLinkedListNode(2, firstNode)
+	assert.Equal(t, 2, secondNode.Val)
+	assert.Equal(t, firstNode, secondNode.Next)
 }
 
-func TestNewSentinelSinglyLinkedListNode(t *testing.T) {
+func TestSinglyLinkedListNode_NewSentinel(t *testing.T) {
 	node := NewSentinelSinglyLinkedListNode[int]()
-	assert.Equal(t, 0, node.val)
-	assert.Nil(t, node.next)
+	assert.Equal(t, 0, node.Val)
+	assert.Nil(t, node.Next)
 }
 
-func TestNewSinglyLinkedList(t *testing.T) {
+func TestSinglyLinkedList_New(t *testing.T) {
 	arr := []int{1, 2, 3, 4, 5}
 
 	headSentinel := NewSinglyLinkedList(arr)
 	assert.NotNil(t, headSentinel)
 
-	cur := headSentinel.next
+	cur := headSentinel.Next
 	for _, num := range arr {
-		assert.Equal(t, num, cur.val)
-		cur = cur.next
+		assert.Equal(t, num, cur.Val)
+		cur = cur.Next
 	}
 }
 
-func TestNewSinglyLinkedList_DifferentGenericTypes(t *testing.T) {
+func TestSinglyLinkedList_DifferentGenericTypes(t *testing.T) {
 	intArr := []int{1, 2, 3, 4, 5}
 	intHeadSentinel := NewSinglyLinkedList(intArr)
-	intCur := intHeadSentinel.next
+	intCur := intHeadSentinel.Next
 	for _, num := range intArr {
-		assert.Equal(t, num, intCur.val)
-		intCur = intCur.next
+		assert.Equal(t, num, intCur.Val)
+		intCur = intCur.Next
 	}
 
 	floatArr := []float64{1.0, 2.0, 3.0, 4.0, 5.0}
 	floatHeadSentinel := NewSinglyLinkedList(floatArr)
-	floatCur := floatHeadSentinel.next
+	floatCur := floatHeadSentinel.Next
 	for _, num := range floatArr {
-		assert.Equal(t, num, floatCur.val)
-		floatCur = floatCur.next
+		assert.Equal(t, num, floatCur.Val)
+		floatCur = floatCur.Next
 	}
 
 	stringArr := []string{"hello", "world", "I'm", "awake"}
 	stringHeadSentinel := NewSinglyLinkedList(stringArr)
-	stringCur := stringHeadSentinel.next
+	stringCur := stringHeadSentinel.Next
 	for _, str := range stringArr {
-		assert.Equal(t, str, stringCur.val)
-		stringCur = stringCur.next
+		assert.Equal(t, str, stringCur.Val)
+		stringCur = stringCur.Next
 	}
 
 	boolArr := []bool{true, false, false, true}
 	boolHeadSentinel := NewSinglyLinkedList(boolArr)
-	boolCur := boolHeadSentinel.next
+	boolCur := boolHeadSentinel.Next
 	for _, str := range boolArr {
-		assert.Equal(t, str, boolCur.val)
-		boolCur = boolCur.next
+		assert.Equal(t, str, boolCur.Val)
+		boolCur = boolCur.Next
 	}
 
 	type person struct {
@@ -72,9 +76,9 @@ func TestNewSinglyLinkedList_DifferentGenericTypes(t *testing.T) {
 		{name: "johny", age: 30},
 	}
 	customStructHeadSentinel := NewSinglyLinkedList(customStructArr)
-	customStructCur := customStructHeadSentinel.next
+	customStructCur := customStructHeadSentinel.Next
 	for _, str := range customStructArr {
-		assert.Equal(t, str, customStructCur.val)
-		customStructCur = customStructCur.next
+		assert.Equal(t, str, customStructCur.Val)
+		customStructCur = customStructCur.Next
 	}
 }
