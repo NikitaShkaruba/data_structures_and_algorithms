a74819338b3ad19060302bb285fcebd0882bf34a
n-shkaruba <n-shkaruba@yandex-team.ru>
2023-06-12 12:47:05 +0200

    Add algorithms/math.go

---
+++ src/algorithms/math.go
@@ -0,0 +1,35 @@
+package algorithms
+
+////////////////////// Basic functions //////////////////////
+
+// ordered can be replaced with constraints.Ordered from "golang.org/x/exp/constraints".
+// But because leetcode.com doesn't allow to use external libs, i've written mine
+type ordered interface {
+	~float32 | ~float64 |
+		~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+func Max[T ordered](a, b T) T {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func Min[T ordered](a, b T) T {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func Abs[T ordered](a T) T {
+	if a < 0 {
+		return -a
+	} else {
+		return a
+	}
+}
---
+++ src/algorithms/math_test.go
@@ -0,0 +1,48 @@
+package algorithms
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMax(t *testing.T) {
+	assert.Equal(t, 4, Max(1, 4))
+	assert.Equal(t, 3, Max(-10, 3))
+	assert.Equal(t, 15, Max(0, 15))
+	assert.Equal(t, 2, Max(-2, 2))
+	assert.Equal(t, -2, Max(-2, -10))
+	assert.Equal(t, 1, Max(1, 1))
+	assert.Equal(t, 123456789, Max(123456789, 123456788))
+}
+
+func TestMax_DifferentGenericTypes(t *testing.T) {
+	assert.Equal(t, 2, Max(1, 2))
+	assert.Equal(t, 2.0, Max(1.0, 2.0))
+}
+
+func TestMin(t *testing.T) {
+	assert.Equal(t, 1, Min(1, 4))
+	assert.Equal(t, -10, Min(-10, 3))
+	assert.Equal(t, 0, Min(0, 15))
+	assert.Equal(t, -2, Min(-2, 2))
+	assert.Equal(t, -10, Min(-2, -10))
+	assert.Equal(t, 1, Min(1, 1))
+	assert.Equal(t, 123456788, Min(123456789, 123456788))
+}
+
+func TestMin_DifferentGenericTypes(t *testing.T) {
+	assert.Equal(t, 1, Min(1, 2))
+	assert.Equal(t, 1.0, Min(1.0, 2.0))
+}
+
+func TestAbs(t *testing.T) {
+	assert.Equal(t, 1, Abs(1))
+	assert.Equal(t, 7, Abs(-7))
+	assert.Equal(t, 0, Abs(0))
+	assert.Equal(t, 123456789, Abs(-123456789))
+}
+
+func TestAbs_DifferentGenericTypes(t *testing.T) {
+	assert.Equal(t, 1, Abs(-1))
+	assert.Equal(t, 1.0, Abs(-1.0))
+}
