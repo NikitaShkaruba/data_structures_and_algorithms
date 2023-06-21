16cbed940e3e8bdf340c8a159a5799c2eb92aaac
n-shkaruba <n-shkaruba@yandex-team.ru>
2023-06-21 21:38:25 +0200

    Refactor math.go a little bit

--- src/stdlib/math.go
+++ src/stdlib/math.go
@@ -2,14 +2,6 @@ package stdlib
 
 ////////////////////// Math functions //////////////////////
 
-// ordered can be replaced with constraints.Ordered from "golang.org/x/exp/constraints".
-// But because leetcode.com doesn't allow to use external libs, i've written mine
-type ordered interface {
-	~float32 | ~float64 |
-	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
-}
-
 func Max[T ordered](a, b T) T {
 	if a > b {
 		return a
@@ -32,4 +24,15 @@ func Abs[T ordered](a T) T {
 	} else {
 		return a
 	}
+}
+
+////////////////////// Ordered //////////////////////
+
+// ordered can be replaced with constraints.Ordered from "golang.org/x/exp/constraints".
+// But because leetcode.com doesn't allow to use external libs, i've written mine
+// Also, I'm copypasting it everywhere for easier copy-pasting
+type ordered interface {
+	~float32 | ~float64 |
+		~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
